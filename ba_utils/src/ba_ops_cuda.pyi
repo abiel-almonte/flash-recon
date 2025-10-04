a71a58512a00995770c1a708ab12e6d3d11a1c96
Abiel Almonte <almonteabiel@gmail.com>
2025-10-04 00:35:00 -0400

    BA csrc pystubs

--- ba_utils/src/ba_ops_cuda.pyi
+++ ba_utils/src/ba_ops_cuda.pyi
@@ -5,7 +5,23 @@ CUDA-accelerated bundle adjustment operations
 from __future__ import annotations
 import torch
 
-__all__: list[str] = ["fused_projective_transform_with_reduction_cuda"]
+__all__: list[str] = [
+    "fused_depth_jacobians_cuda",
+    "fused_projective_transform_with_reduction_cuda",
+]
+
+def fused_depth_jacobians_cuda(
+    disps: torch.Tensor,
+    mono_disps: torch.Tensor,
+    valid_depth: torch.Tensor,
+    scales: torch.Tensor,
+    shifts: torch.Tensor,
+    ignore: torch.Tensor,
+    alpha: float,
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
+    Fused operation to compute depth jacobians
+    """
 
 def fused_projective_transform_with_reduction_cuda(
     t: torch.Tensor,
@@ -16,7 +32,7 @@ def fused_projective_transform_with_reduction_cuda(
     jj: torch.Tensor,
     target: torch.Tensor,
     weight: torch.Tensor,
-) -> list[torch.Tensor]:
+) -> tuple[torch.Tensor, torch.Tensor]:
     """
     Fused projective transform with reduction (iproj+SE3+proj)
     """
