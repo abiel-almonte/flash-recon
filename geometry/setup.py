5a3ad0acd24c0ef801d705393b908b7b62799606
Abiel Almonte <almonteabiel@gmail.com>
2025-10-12 10:02:39 -0400

    create geometry folder

---
+++ geometry/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import CUDAExtension, BuildExtension
+
+common_includes = ["csrc", "csrc/common"]
+
+
+def create_module(name):
+    return CUDAExtension(
+        name=f"geometry_cuda.{name}",
+        sources=[
+            f"csrc/{name}/bindings.cpp",
+            f"csrc/{name}/kernels.cu",
+        ],
+        include_dirs=[f"csrc/{name}", *common_includes],
+        extra_compile_args={
+            "cxx": ["-O3", "-std=c++17"],
+            "nvcc": ["-O3", "--use_fast_math", "-lineinfo"],
+        },
+    )
+
+
+setup(
+    name="geometry_cuda",
+    version="0.1.0",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    ext_modules=[create_module(m) for m in ["ba", "proj", "lie"]],
+    cmdclass={"build_ext": BuildExtension},
+    install_requires=["torch"],
+)
