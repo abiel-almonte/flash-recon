150f50da05718bfc39cf88f2f8fe2417eae164ee
Abiel Almonte <almonteabiel@gmail.com>
2025-10-14 23:40:34 -0400

    formatting

--- neural/src/neural_cuda/corr.pyi
+++ neural/src/neural_cuda/corr.pyi
@@ -1,7 +1,11 @@
 from __future__ import annotations
 import torch
-__all__: list[str] = ['corr_forward']
-def corr_forward(volume: torch.Tensor, coords: torch.Tensor, radius: int) -> torch.Tensor:
+
+__all__: list[str] = ["corr_forward"]
+
+def corr_forward(
+    volume: torch.Tensor, coords: torch.Tensor, radius: int
+) -> torch.Tensor:
     """
     Compute correlations
     """
