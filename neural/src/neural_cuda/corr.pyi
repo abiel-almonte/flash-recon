94df38da114ba7a74e514c832e01905f9c50352b
Abiel Almonte <almonteabiel@gmail.com>
2025-10-14 23:29:51 -0400

    create python module for corrblock

---
+++ neural/src/neural_cuda/corr.pyi
@@ -0,0 +1,7 @@
+from __future__ import annotations
+import torch
+__all__: list[str] = ['corr_forward']
+def corr_forward(volume: torch.Tensor, coords: torch.Tensor, radius: int) -> torch.Tensor:
+    """
+    Compute correlations
+    """
