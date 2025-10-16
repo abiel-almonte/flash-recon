03becffd1601f4e6db867054a12fcba3a7f3f05d
Abiel Almonte <almonteabiel@gmail.com>
2025-10-16 00:02:34 -0400

    add command to build neural csrc

--- build_modules.sh
+++ build_modules.sh
@@ -5,5 +5,8 @@ pip install --no-build-isolation -e /workspace/geometry/ \
 && pybind11-stubgen geometry_cuda.proj -o /workspace/geometry/src \
 && pybind11-stubgen geometry_cuda.lie -o /workspace/geometry/src
 
+pip install --no-build-isolation -e /workspace/neural/ \
+&& pybind11-stubgen neural_cuda.corr -o /workspace/neural/src
+
 pip install --no-build-isolation -e /workspace/vision_rt/ \
 && pybind11-stubgen vision_rt_cuda -o /workspace/vision_rt/src
