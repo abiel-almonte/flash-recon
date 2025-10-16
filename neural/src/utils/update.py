c30843a7cea782a19b3c6f9974b2516c6c5947b6
Abiel Almonte <almonteabiel@gmail.com>
2025-10-15 23:14:35 -0400

    formatting

--- neural/src/utils/update.py
+++ neural/src/utils/update.py
@@ -4,10 +4,11 @@ import torch.nn as nn
 from .gru import ConvGRU
 from .graph_agg import GraphAgg
 
+
 class UpdateModule(nn.Module):
     def __init__(self):
         super(UpdateModule, self).__init__()
-        cor_planes = 4 * (2*3+1)**2
+        cor_planes = 4 * (2 * 3 + 1) ** 2
 
         self.corr_encoder = nn.Sequential(
             nn.Conv2d(cor_planes, 128, kernel_size=(1, 1), padding=(0, 0)),
@@ -36,11 +37,11 @@ class UpdateModule(nn.Module):
             nn.Conv2d(128, 2, kernel_size=(3, 3), padding=(1, 1)),
         )
 
-        self.gru = ConvGRU(128, 128+128+64)
+        self.gru = ConvGRU(128, 128 + 128 + 64)
         self.agg = GraphAgg()
 
     def forward(self, net, inp, corr, flow=None, ii=None, jj=None):
-        """ update operation """
+        """update operation"""
 
         batch, num, ch, ht, wd = net.shape
         device = net.device
@@ -50,10 +51,10 @@ class UpdateModule(nn.Module):
 
         out_dim = (batch, num, -1, ht, wd)
 
-        net = net.view(batch*num, -1, ht, wd)
-        inp = inp.view(batch*num, -1, ht, wd)
-        corr = corr.view(batch*num, -1, ht, wd)
-        flow = flow.view(batch*num, -1, ht, wd)
+        net = net.view(batch * num, -1, ht, wd)
+        inp = inp.view(batch * num, -1, ht, wd)
+        corr = corr.view(batch * num, -1, ht, wd)
+        flow = flow.view(batch * num, -1, ht, wd)
 
         corr = self.corr_encoder(corr)
         flow = self.flow_encoder(flow)
