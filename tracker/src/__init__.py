c2e096a22638fadbedf77e26e007a1cd7ba6701a
Abiel Almonte <almonteabiel@gmail.com>
2025-10-16 18:01:28 -0400

    move dsp optimizer to tracker folder

--- tracker/src/__init__.py
+++ tracker/src/__init__.py
@@ -1,7 +1,9 @@
+from .keyframe_buffer import KeyFrameBuffer
 from .dsp_optimizer import DSPOptimizer
 from .structs import OptimizationPayload, CallerRole
 
 __all__ = [
+    "KeyFrameBuffer",
     "DSPOptimizer",
     "OptimizationPayload", 
     "CallerRole"
