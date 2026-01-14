90770ad49576c55fdcedaf32adc03bf9fd6fdd6d
Abiel Almonte <almonteabiel@gmail.com>
2026-01-13 22:53:37 -0500

    update benchmarking imports

--- benchmark/new/update_pose.py
+++ benchmark/new/update_pose.py
@@ -1,11 +1,10 @@
 import torch
 
-from pose_utils import (
+from geometry import (
     Pose,
     Tangent,
     pose_retraction,
-    matrix_to_quat_cuda,
-    quat_to_matrix_cuda,
+    matrix_to_pose
 )
 
 
@@ -17,14 +16,13 @@ def update_pose(camera, converged_threshold=1e-4):
     phi = camera.cam_rot_delta.unsqueeze(0)  # [1, 3]
     tangent = Tangent(rho, phi)
 
-    current_q = matrix_to_quat_cuda(camera.R.unsqueeze(0))  # [1, 4]
-    current_pose = Pose(camera.T.unsqueeze(0), current_q)  # [1, 3], [1, 4]
+    current_pose = matrix_to_pose(camera.R.unsqueeze(0))
 
     new_pose = pose_retraction(current_pose, tangent)
 
-    new_R = quat_to_matrix_cuda(new_pose.q)[0]  # [3, 3]
+    #new_R = quat_to_matrix_cuda(new_pose.q)[0]  # [3, 3]
     new_T = new_pose.t[0]  # [3]
-    camera.update_RT(new_R, new_T)
+    #camera.update_RT(new_R, new_T)
 
     tau = torch.cat([camera.cam_trans_delta, camera.cam_rot_delta])
     converged = tau.norm() < converged_threshold
