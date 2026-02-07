8222570cc7bfacaac195692edebe8463e3bb5c6a
Abiel Almonte <almonteabiel@gmail.com>
2026-02-07 02:21:01 +0000

    Create frame distance cuda kernel

--- geometry/csrc/proj/bindings.cpp
+++ geometry/csrc/proj/bindings.cpp
@@ -73,4 +73,17 @@ PYBIND11_MODULE(proj, m) {
         py::arg("thresh"),
         "Count number of neighboring depths are consistent"
     );
+
+    m.def(
+        "frame_distance_cuda",
+        &frame_distance_cuda,
+        py::arg("t"),
+        py::arg("q"),
+        py::arg("disps"),
+        py::arg("intrinsics"),
+        py::arg("ii"),
+        py::arg("jj"),
+        py::arg("beta"),
+        "Compute reprojection-based frame distance"
+    );
 }
