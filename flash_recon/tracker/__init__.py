dde5776fee68394f19a04c83e1c0d35e8caa8334
Abiel Almonte <almonteabiel@gmail.com>
2026-02-07 01:23:23 +0000

    Refactor __init__.py to remove unused imports and update exported symbols, replacing them with SLAM class.

--- flash_recon/tracker/__init__.py
+++ flash_recon/tracker/__init__.py
@@ -1,14 +1,3 @@
-from .keyframe_buffer import KeyFrameBuffer
-from .dsp_optimizer import DSPOptimizer
-from .factor_graph import FactorGraph
-from .structs import OptimizationPayload, ProximityPayload, BufferPayload, CallerRole
+from .slam import SLAM
 
-__all__ = [
-    "KeyFrameBuffer",
-    "DSPOptimizer",
-    "FactorGraph",
-    "OptimizationPayload",
-    "ProximityPayload",
-    "BufferPayload",
-    "CallerRole",
-]
+__all__ = ["SLAM"]
