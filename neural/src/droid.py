2fc3582aee530a0cf0caf7cc108ce389b8bdfdc7
Abiel Almonte <almonteabiel@gmail.com>
2026-02-07 08:26:18 +0000

    Update apply_cnet method to return split outputs for net and inp

--- neural/src/droid.py
+++ neural/src/droid.py
@@ -38,7 +38,8 @@ class DroidNet(nn.Module):
         return DroidNet._apply_module(self.fnet, inputs)
 
     def apply_cnet(self, *inputs):
-        return DroidNet._apply_module(self.cnet, inputs)
+        x = DroidNet._apply_module(self.cnet, inputs)
+        return x.split([128, 128], dim=1)  # net, inp
 
     def apply_update(self, *inputs):
         outputs_b = DroidNet._apply_module(self.update, tuple(x.half() for x in inputs))
