1490337e5511d21b30d799f00c1f069c91abd46e
Abiel Almonte <almonteabiel@gmail.com>
2026-02-09 23:31:32 +0000

    Add altcorr forward function and include necessary headers

--- neural/csrc/corr/kernels.cu
+++ neural/csrc/corr/kernels.cu
@@ -1,5 +1,7 @@
 #include "kernels.h"
 #include "kernels/corr.cuh"
+#include "kernels/altcorr.cuh"
+
 
 Tensor corr_forward(
     const Tensor volume, // [T, H, W, Hi, Wi]
@@ -34,3 +36,39 @@ Tensor corr_forward(
     return corr;
 }
 
+
+Tensor altcorr_forward(
+    const Tensor fmap1,
+    const Tensor fmap2,
+    const Tensor coords,
+    int radius
+) { 
+    CHECK_INPUT(fmap1);
+    CHECK_INPUT(fmap2);
+    CHECK_INPUT(coords);
+
+    const int T = coords.size(0);
+    const int H = coords.size(2);
+    const int W = coords.size(3);
+
+    const int rd = 2 * radius + 1;
+    
+    auto opts = fmap1.options();
+    Tensor corr = torch::zeros({T, rd * rd, H, W}, opts);
+
+    const dim3 blocks(T, (H + 4 - 1) / 4, (W + 8 - 1) / 8);
+    const dim3 threads(4, 8);
+      
+    AT_DISPATCH_FLOATING_TYPES_AND_HALF(fmap1.scalar_type(), "altcorr_forward_kernel", [&] {
+        altcorr_forward_kernel<scalar_t><<<blocks, threads>>>(
+            fmap1.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>(),
+            fmap2.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>(),
+            coords.packed_accessor32<float,4,torch::RestrictPtrTraits>(),
+            corr.packed_accessor32<float,4,torch::RestrictPtrTraits>(),
+            radius
+        );
+      }
+    );
+
+    return corr;
+}
