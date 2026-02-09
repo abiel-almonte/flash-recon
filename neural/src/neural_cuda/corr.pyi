4022b03cfebc9c61f67ed7465f67ab1bbc334b65
Abiel Almonte <almonteabiel@gmail.com>
2026-02-09 23:33:18 +0000

    Add altcorr_forward to py stubs

--- neural/src/neural_cuda/corr.pyi
+++ neural/src/neural_cuda/corr.pyi
@@ -1,7 +1,14 @@
 from __future__ import annotations
 import torch
 
-__all__: list[str] = ["corr_forward"]
+__all__: list[str] = ["altcorr_forward", "corr_forward"]
+
+def altcorr_forward(
+    fmap1: torch.Tensor, fmap2: torch.Tensor, coords: torch.Tensor, radius: int
+) -> torch.Tensor:
+    """
+    Compute correlations on the fly
+    """
 
 def corr_forward(
     volume: torch.Tensor, coords: torch.Tensor, radius: int
