6f7d3f1c734ab9a376462b7896f2e1b7e08a7465
Abiel Almonte <almonteabiel@gmail.com>
2026-02-09 23:31:41 +0000

    Add declaration for altcorr_forward function

--- neural/csrc/corr/kernels.h
+++ neural/csrc/corr/kernels.h
@@ -2,4 +2,15 @@
 #include "common/common.h"
 
 
-Tensor corr_forward(const Tensor volume, const Tensor coords, const int radius);
+Tensor corr_forward(
+    const Tensor volume,
+    const Tensor coords,
+    const int radius
+);
+
+Tensor altcorr_forward(
+    const Tensor fmap1,
+    const Tensor fmap2,
+    const Tensor coords,
+    const int radius
+);
