0aa9e2ec8a6aebfb982e74180b720f88eb6fdd4c
Abiel Almonte <almonteabiel@gmail.com>
2026-02-15 23:08:12 +0000

    Add SLAMSnapshot and KeyFrame dataclasses for interoperating between slam and gsplat

--- flash_recon/slam/utils/contexts.py
+++ flash_recon/slam/utils/contexts.py
@@ -1,3 +1,7 @@
+import torch
+
+from geometry import Pose, Intrinsics
+
 from dataclasses import dataclass
 from typing import Optional
 from .enums import BAType
@@ -51,3 +55,35 @@ class BufferSnapshot:
     fmaps: Optional[object] = None
     nets: Optional[object] = None
     inps: Optional[object] = None
+
+
+@dataclass
+class SLAMSnapshot:
+    poses: Optional[Pose] = None
+    disps: Optional[torch.Tensor] = None
+    vmask: Optional[torch.Tensor] = None
+    intrinsics: Optional[Intrinsics] = None
+    version: Optional[object] = None
+
+    @property
+    def n_keyframes(self):
+        return self.disps.size(0)
+
+    def copy(self):
+        return SLAMSnapshot(
+            poses=self.poses.clone(),
+            disps=self.disps.clone(),
+            intrinsics=self.intrinsics,
+            version=self.version,
+        )
+
+
+@dataclass
+class KeyFrame:
+    frame: Optional[object] = None
+    poses: Optional[object] = None
+    disps: Optional[object] = None  # disps up
+    vmask: Optional[object] = None  # vmask up
+
+    def add_frame(self, frame) -> None:
+        self.frame = frame
