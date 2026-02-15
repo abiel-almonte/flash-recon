6a72a850c71e490665d49354d298f634e2c12b63
Abiel Almonte <almonteabiel@gmail.com>
2026-02-15 23:06:19 +0000

    Expose splat utils

---
+++ flash_recon/splat/utils/__init__.py
@@ -0,0 +1,5 @@
+from .gaussian_buffer import GaussianBuffer
+from .gaussian_optimizer import GaussianOptimizer
+from .structs import SplatSnapshot
+from .backproject import backproject
+from .deform import deform_gaussians
