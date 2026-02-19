17bcac27c9b22edd0f7f7136fba78ee1f8d63611
Abiel Almonte <almonteabiel@gmail.com>
2026-02-19 20:44:33 +0000

    formatting

--- geometry/src/__init__.py
+++ geometry/src/__init__.py
@@ -11,7 +11,7 @@ from .lie import (
     pose_to_matrix,
     matrix_to_pose,
     quat_multiply_cuda,
-    quat_to_matrix_cuda
+    quat_to_matrix_cuda,
 )
 
 from .utils import get_meshgrid, iproj
